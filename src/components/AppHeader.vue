7cb0cbeea8c837611324bccd9d0e63837866bcee
gluke0 <milanesigianluca@gmail.com>
2023-06-30 15:04:10 +0200

    active link header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,7 +22,7 @@ export default{
 
     <div>
 
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <nav class="navbar navbar-expand-sm navbar-light bg-dark">
               <div class="container">
                 <router-link :to="{name: 'home'}" > <img src="/img/logo.png" style="width: 50%" class="mx-0 px-0" alt="gluke logo"> </router-link>
                 <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
@@ -32,10 +32,10 @@ export default{
                 <div class="collapse navbar-collapse" id="collapsibleNavId">
                     <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                         <li class="nav-item">
-                            <router-link class="navbar-brand" :to="{name: 'home'}"> Home </router-link>
+                            <router-link class="navbar-brand items" :to="{name: 'home'}"> Home </router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link class="navbar-brand" :to="{name: 'portfolio'}"> Portfolio </router-link>
+                            <router-link class="navbar-brand items" :to="{name: 'portfolio'}"> Portfolio </router-link>
                         </li>
                     </ul>
                 </div>
@@ -49,6 +49,13 @@ export default{
 
 <style lang="scss">
 
+.items{
+  color: white !important;
+}
+
+.router-link-active{
+  color: rgb(139, 235, 144) !important;
+}
 
 </style>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'Vue'
+import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 
