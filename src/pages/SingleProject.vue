95078cd017dc2a89af45a977a9d7320a2644438a
gluke0 <milanesigianluca@gmail.com>
2023-07-03 18:27:03 +0200

    fix

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -53,10 +53,11 @@ export default{
     <div class="card text-white bg-primary p-5">
       <h4 class="card-title"> <a class="text-decoration-none" :href="project.link"> {{ project.title }} </a>  </h4>
       <img class="img-fluid my-3" :src="`${commonUrl}/storage/${project.image}`" alt="">
-      <div > <i class="fa-solid fa-info me-4"></i> {{ project.category.name }}</div>
+      <div > <i class="fa-solid fa-info me-4" ></i> {{ project.category.name }}</div>
       <div class="my-3"> <i class="fa-regular fa-file-lines me-3"></i> {{ project.description }}</div>
       <li class="list-inline" v-for="(project, index) in project.technologies" :key="index"> <i class="fa-solid fa-laptop-code me-2"></i> {{ project.name }}</li>
-      <div class="mt-3"> <i class="fa-solid fa-link me-2"></i> <a class="text-decoration-none" :href="project.link"> Get from GitHub </a></div>
+      <div class="mt-3"> <i class="fa-solid fa-link me-2"></i> <a class="text-decoration-none" :href="project.link"> Get from GitHub </a>
+      </div>
     </div>
   </div>
 </main>
